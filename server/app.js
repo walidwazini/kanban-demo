f4232a33e4081100ddfda191c4ca60c0ca47e961
Walid Wazini <wazini25@gmail.com>
2022-08-09 16:03:57 +0800

    first commit

---
+++ server/app.js
@@ -0,0 +1,24 @@
+const express = require('express');
+const path = require('path');
+const cookieParser = require('cookie-parser');
+const logger = require('morgan');
+const cors = require('cors')
+
+const indexRouter = require('./src/v1/routes/index');
+const usersRouter = require('./src/v1/routes/users');
+
+const app = express();
+
+app.use(cors())
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use('/api/v1', require('./src/v1/routes'))
+
+// app.use('/', indexRouter);
+// app.use('/users', usersRouter);
+
+module.exports = app;
