2e68bdc1375885e8e9b80e35f46b79bb9ac5b5b9
Walid Wazini <wazini25@gmail.com>
2022-08-13 09:19:02 +0800

    getSingleBoard n editBoard req

--- server/src/v1/controllers/board-ctrl.js
+++ server/src/v1/controllers/board-ctrl.js
@@ -41,3 +41,65 @@ exports.updatePosition = async (req, res) => {
     res.status(500).json(err)
   }
 }
+
+exports.getOne = async (req, res) => {
+  const { boardId } = req.params
+
+  try {
+    const board = await Board.findOne({ user: req.user._id, _id: boardId })
+    if (!board) return res.status(404).json('Board not found')
+
+    const sections = await Section.find({ board: boardId })
+    for (const section of sections) {
+      const tasks = await Task.find({ section: section.id })
+        .populate('section')
+        .sort('-position')
+      section._doc.tasks = tasks
+    }
+    board._doc.sections = sections
+    res.status(200).json(board)
+
+  } catch (err) {
+    res.status(500).json(err)
+  }
+}
+
+exports.editBoard = async (req, res) => {
+  const { boardId } = req.params
+  const { title, description, favourite } = req.body
+
+  try {
+    if (title === '') req.body.title = 'Untitled'
+    if (description === '') req.body.description = 'Add description here'
+    const currentBoard = await Board.findById(boardId)
+    if (!currentBoard) return res.status(404).json('Board not found')
+
+    if (favourite !== undefined && currentBoard.favourite !== favourite) {
+      const favourites = await Board.find({
+        user: currentBoard.user,
+        favourite: true,
+        _id: { $ne: boardId }
+      })
+      if (favourite) {
+        req.body.favouritePosition = favourites.length > 0 ? favourites.length : 0
+      } else {
+        for (const key in favourites) {
+          const element = favourites[key]
+          await Board.findOneAndUpdate(
+            element.id,
+            { $set: { favouritePosition: key } }
+          )
+        }
+      }
+    }
+
+    const board = await Board.findByIdAndUpdate(
+      boardId,
+      { $set: req.body },
+    )
+    res.status(200).json(board)
+
+  } catch (err) {
+    res.status(500).json(err)
+  }
+}
--- server/src/v1/routes/board.routes.js
+++ server/src/v1/routes/board.routes.js
@@ -3,6 +3,7 @@ const { param } = require('express-validator')
 const validation = require('../handlers/validation')
 const tokenHandler = require('../handlers/tokenHandler')
 const boardController = require('../controllers/board-ctrl')
+const { validate } = require('../models/user')
 
 router.post('/', tokenHandler.verifyToken, boardController.create)
 
@@ -16,6 +17,30 @@ router.put(
   '/',
   tokenHandler.verifyToken,
   boardController.updatePosition
+)
+
+router.get(
+  '/:boardId',
+  param('boardId').custom(value => {
+    if (!validation.isObjectId(value)) {
+      return Promise.reject('invalid id')
+    } else return Promise.resolve()
+  }),
+  validation.validate,
+  tokenHandler.verifyToken,
+  boardController.getOne
+)
+
+router.put(
+  '/:boardId',
+  param('boardId').custom(value => {
+    if (!validation.isObjectId(value)) {
+      return Promise.reject('invalid id')
+    } else return Promise.resolve()
+  }),
+  validation.validate,
+  tokenHandler.verifyToken,
+  boardController.editBoard
 )
 
 
